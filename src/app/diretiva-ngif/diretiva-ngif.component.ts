f1c6a0c90fb67016733bc97d30234296c9096a74
Alexandre Silva <alexandre.b.silva@capgemini.com>
2022-10-18 09:50:19 -0300

    Diretivas + Angular CLI class

---
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.scss']
+})
+export class DiretivaNgifComponent implements OnInit {
+
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {
+  }
+
+}
